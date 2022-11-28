84483f6b9ca42df1dcb28139af8474a5b01658eb
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-28 09:52:26 -0300

    content for first part

--- src/page-structure/Content.tsx
+++ src/page-structure/Content.tsx
@@ -20,12 +20,26 @@ const Content = () => {
         <div className='h-[1px] w-[50%] m-auto bg-black mt-[.5rem] mb-[.3rem]'></div>
         <p className='text-[1.2rem] italic font-semibold text-center'>A client said:</p>
         <p className='text-center'>"It is not possible he is only 14! My son who is 23 y/o couldnt fix it. The boy did a great job."</p>
+        <div className='h-[1px] w-[30%] m-auto bg-black mt-[.5rem] mb-[.3rem]'></div>
+        <p className='text-[.9rem] text-center font-semibold mb-[.1rem]'>The benefits of sleeping well</p>
+        <p className='text-[.8rem] mr-[.4rem]'>Good sleep improves your brain performance, mood, and health. Not getting enough quality sleep regularly raises the risk of many diseases and disorders. There range from heart disease and stroke to obesity and dementia. There's more to good sleep than just the hours spent in bed, says Dr. Marishka Brown.</p>
+        <br></br>
+        <p className='text-[.8rem] mr-[.4rem]'>Healthy sleep encompasses three major things. One is how much sleep you get. Another is sleep quality — that you get uninterrupted and refreshing sleep. The last is a consistent sleep schedule.</p>
+        <br></br>
+        <p className='text-[.8rem] mr-[.4rem]'>There are many things you can do to improve your sleep[...]</p>
+        <p className='text-[.8rem] text-right mr-[.4rem]'>~quoted from NIH</p>
       </div>
       <div>
         <p className='text-center border-2 bg-black text-white h-[2rem] flex justify-center items-center border-black'>Something important</p>
         <p className='ml-[.5rem] mt-[.3rem]'>Throughout the years he spent working as a Media Analyst, <em className='font-semibold'>Renan Araújo</em> had the opportunity to work with big companies in Brazil and abroad.</p><br></br>
         <p>He spent years creating marketing strategies, campaigns, and a lot more for schools, universities and other businesses around the world. Such as: Middlesex University Dubai, Red Balloon, Unimes, Amazing Explorers, Jeep Prime Rio and LifeSound.</p><br></br>
         <p>He developed a very analytical behavior, that contributed a lot for him to become a <em>solid</em> and creative web developer.</p>
+        <div className='h-[1px] w-[30%] m-auto bg-black mt-[.5rem] mb-[.3rem]'></div>
+        <p className='text-[.9rem] font-semibold text-center'>The Power of Effort</p>
+        <p className='text-[.8rem]'>Effort teaches us that we can do more than we thought. We learn new skills and abilities when we take the time to put in consistent effort. Every new success that we achieve through our efforts <em className='not-italic font-semibold'>increases our confidence in ourselves and our abilities.</em> Effort keeps ur focuses on success.</p>
+        <p className='text-[.8rem]'>If our efforts lead us further away from our goal, then we need to change our strategy. If our efforts brings us closer to our goals, we can double down with our effort.</p>
+        <p className='text-[.8rem]'>As your effort brings you closer to your goal, your motivation and willpower increase as you can taste victory.</p>
+        <p className='text-[.8rem]'>Whatever your goal, plan to put in the consistent effort needed to achieve success.</p>
       </div>
     </div>
   </>)
