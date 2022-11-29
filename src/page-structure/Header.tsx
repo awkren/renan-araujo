aacf1cfda8d7ed718b88edfdd09050ab895c6a61
Renan Araújo <renan.araujoonline@gmail.com>
2022-11-29 13:41:43 -0300

    added text below NP title

--- src/page-structure/Header.tsx
+++ src/page-structure/Header.tsx
@@ -3,19 +3,30 @@ import '../page-style/Header.css'
 
 const Header = () => {
   return (<>
-    <div className='flex flex-row'>
-      <div className='flex justify-center items-center'>
-        <p className='w-[80%] text-[.7rem] text-center'><em className='font-bold not-italic'>this is some random text,</em><br></br> if i were you, i wouldnt bother reading this, but if you want go aheada asdsad asdas dsa, keep up if you want </p>
+
+    <div className='flex flex-row justify-between'>
+      <div className='w-[20%]'>
+        <p className='text-right'>this is some random text, if i were you, i wouldnt bother reading this, but if you want go aheada asdsad asdas dsa dsadasdasdasdasdasdas</p>
       </div>
-      <div id=''className='flex justify-center items-center w-[100%] text-center'>
-        <p className='text-[4.4rem] font-[Chomsky]'>The Front-End Developer</p>
+      <div id=''className='flex text-center'>
+        <p className='text-[5rem] font-[Chomsky]'>The Front-End Developer</p>
       </div>
-      <div className='flex justify-center items-center '> 
-        <p className='w-[50%] text-[.7rem] text-center border-2 border-black p-1'>this is some random text, if i were you, i wouldnt bother reading this, but if you want go aheada asdsad asdas dsa dsadasdasdasdasdasdas</p>
+      <div className='w-[20%]'> 
+        <p className=''>this is some random text, if i were you, i wouldnt bother reading this, but if you want go aheada asdsad asdas dsa dsadasdasdasdasdasdas</p>
       </div>
     </div>
+
       <div className='h-[.2rem] bg-black'></div>
-      <p className='text-[1rem]'>adasdasdsad</p>
+      <div className='flex flex-row justify-evenly'>
+      <p className=''>Rio de Janeiro</p>
+      <p>Brazil</p>
+      <p>Flamengo</p>
+      <p>Cleveland Browns</p>
+      <p>Cleveland Cavaliers</p>
+      <p>Funk</p>
+      <p>Rap</p>
+      <p>Country Music</p>
+      </div>
       <div className='h-[.2rem] bg-black'></div>
   </>)
 }
